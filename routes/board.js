2de2560ded0021b77afdf32b847fbabc1d619f7f
jojojojo1322 <bobob935@gmail.com>
2020-01-20 18:02:48 +0900

    'board-write'

--- routes/board.js
+++ routes/board.js
@@ -16,5 +16,27 @@ router.get('/list/:page', function(req, res, next) {
 router.get('/list', function(req,res,next){
     res.redirect('/board/list/1');
 });
+
+router.get('/write', function(req,res,next){
+    res.render('write',{title: "게시판글쓰기"});
+});
+
+router.post('/write', function(req,res,next){
+    var name = req.body.name;
+    var title = req.body.title;
+    var content = req.body.content;
+    var passwd = req.body.passwd;
+    var datas = [name,title,content,passwd];
+
+
+    var sql = "insert into board(name, title, content, regdate, modidate, passwd,hit) values(?,?,?,now(),now(),?,0)";
+    conn.query(sql,datas, function (err, rows) {
+        if (err) console.error("err : " + err);
+        res.redirect('/board/list');
+    });
+});
+
+
+
 module.exports = router;
 
