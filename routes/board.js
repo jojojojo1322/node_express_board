1a3ec59508ed6433cc3617ca8bcd153bc44eee1e
jojojojo1322 <bobob935@gmail.com>
2020-01-20 18:14:23 +0900

    'board-paging'

--- routes/board.js
+++ routes/board.js
@@ -73,6 +73,18 @@ router.post('/update',function(req,res,next)
     });
 });
 
+router.get('/page/:page',function(req,res,next)
+{
+    var page = req.params.page;
+    var sql = "select idx, name, title, date_format(modidate,'%Y-%m-%d %H:%i:%s') modidate, " +
+        "date_format(regdate,'%Y-%m-%d %H:%i:%s') regdate,hit from board";
+    conn.query(sql, function (err, rows) {
+        if (err) console.error("err : " + err);
+        res.render('page', {title: ' 게시판 리스트', rows: rows, page:page, length:rows.length-1, page_num:10, pass:true});
+        console.log(rows.length-1);
+    });
+});
+
 
 
 module.exports = router;
